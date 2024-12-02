22cc836fdd6237a817ed63db21aec9923a7d5635
teppolap <teppo.lappalainen28@gmail.com>
2024-12-02 10:40:32 +0200

    optimized profile pic loading

--- src/App.js
+++ src/App.js
@@ -93,10 +93,10 @@ function App() {
           {/* Left Side Header */}
           <header className="w-full lg:w-1/2 lg:sticky lg:top-0 lg:flex lg:flex-col lg:justify-between lg:py-24 bg-gray-900 flex-shrink-0">
             <div className="text-center lg:text-left px-4 py-6 lg:px-8 lg:py-8 flex flex-col items-center lg:items-start">
-                <img
-                src={profilePic}
-                alt="Teppo Lappalainen"
-                className="w-40 h-40 sm:w-60 sm:h-60 md:w-72 md:h-72 lg:w-80 lg:h-80 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 mb-4"
+              <img
+              src={profilePic}
+              alt="Teppo Lappalainen"
+              className="w-40 h-40 sm:w-60 sm:h-60 md:w-72 md:h-72 lg:w-80 lg:h-80 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 mb-4"
               />
               <h1 className="text-3xl font-bold tracking-tight text-slate-200 sm:text-4xl">
                 Teppo Lappalainen
